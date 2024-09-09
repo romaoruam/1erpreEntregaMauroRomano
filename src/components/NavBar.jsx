322b20fa310c1df9e6c99d5234a2bcb9bc02210f
Mauro Romano <roma.letitbleed@hotmail.com>
2024-09-08 21:23:50 -0300

    modifique NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,22 @@
 // src/components/NavBar.jsx
 import React from 'react';
-import CartWidget from './CartWidget'; // Importa CartWidget
+import CartWidget from './CartWidget';
+import './NavBar.css'; // Importa el archivo de estilos específico para NavBar
 
 const NavBar = () => {
     return (
-        <nav>
-            <h1>Mi Tienda</h1> {/* Nombre de la tienda */}
-            <ul>
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <h1>Mi Tienda</h1> {/* Nombre de la tienda */}
+            </div>
+            <ul className="navbar-menu">
                 <li><a href="#home">Inicio</a></li>
                 <li><a href="#shop">Tienda</a></li>
                 <li><a href="#contact">Contacto</a></li>
             </ul>
-            <CartWidget /> {/* Renderiza CartWidget */}
+            <div className="navbar-cart">
+                <CartWidget /> {/* Componente del carrito */}
+            </div>
         </nav>
     );
 };
