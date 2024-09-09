c7c2a6bb1674427abee9b5dad9c4022f996101d0
Mauro Romano <roma.letitbleed@hotmail.com>
2024-09-08 21:31:54 -0300

    modifique estilos

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,12 @@
 // src/components/CartWidget.jsx
 import React from 'react';
+import './CartWidget.css'; // Importa el archivo de estilos específico para CartWidget
 
 const CartWidget = () => {
     const itemCount = 5; // Número de elementos en el carrito (hardcodeado por ahora)
-    
+
     return (
-        <div>
+        <div className="cart-widget">
             <i className="cart-icon">🛒</i> {/* Icono del carrito */}
             <span>{itemCount}</span> {/* Número de elementos en el carrito */}
         </div>
